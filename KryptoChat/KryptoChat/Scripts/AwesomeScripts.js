ae0a747dc49c01437af3cc1e382dbc65b672843d
Alexander Andersson <alexander.andersson.9347@gmail.com>
2017-01-05 15:01:34 +0100

    scroll down shit

--- KryptoChat/KryptoChat/Scripts/AwesomeScripts.js
+++ KryptoChat/KryptoChat/Scripts/AwesomeScripts.js
@@ -17,39 +17,98 @@
     });
 });
 
+//$('#loadMsg').click(function () {
+//    $.post('/Message/GetMessage/',
+//    function (data) {
+//        if (data.Result) {
+//            for (let i = 0; i < data.Result.length; i++) {
+//                if (sessionStorage.getItem("Time") != null)
+//                {
+//                    if (data.Result[i].Timestamp > sessionStorage.Time) {
+//                        if (sessionStorage.Username == data.Result[i].Username) {
+//                            $('#showMsg').append(data.Result[i].Message + '\n');
+//                            //sessionStorage.Time = data.Result[i].Timestamp;
+//                        }
+//                        else {
+//                            var date = new Date(parseInt(data.Result[i].Timestamp.substr(6)));
+//                            $('#showMsg').append('\n' + data.Result[i].Username + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '\n' + data.Result[i].Message + '\n');
+//                            sessionStorage.Username = data.Result[i].Username;
+//                            //sessionStorage.Time = data.Result[i].Timestamp;
+//                        }
+//                    }
+//                }
+//                else
+//                {
+//                    if (sessionStorage.Username == data.Result[i].Username) {
+//                        $('#showMsg').append(data.Result[i].Message + '\n');
+//                        //sessionStorage.Time = data.Result[i].Timestamp;
+//                    }
+//                    else {
+//                        var date = new Date(parseInt(data.Result[i].Timestamp.substr(6)));
+//                        $('#showMsg').append('\n' + data.Result[i].Username + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '\n' + data.Result[i].Message + '\n');
+//                        sessionStorage.Username = data.Result[i].Username;
+//                        //sessionStorage.Time = data.Result[i].Timestamp;
+//                    }
+//                }             
+//            }
+//            sessionStorage.Time = data.Result[data.Result.length - 1].Timestamp;
+//        }
+//        else {
+//            alert("Error");
+//        }
+//    }, 'json');
+//});
+
+
 $('#loadMsg').click(function () {
+
+    var div = $('.chatContainer');
+    var isBottom = false;
+
+    //alert("scrollTop: " + div[0].scrollTop + "\n" +
+    //        "scrollHeight: " + div[0].scrollHeight);
+
+    if (div.scrollTop == div[0].scrollHeight)
+    {
+        isBottom = true;
+    }
+    else
+    {
+        isBottom = false;
+    }
+
+    
+
     $.post('/Message/GetMessage/',
     function (data) {
         if (data.Result) {
             for (let i = 0; i < data.Result.length; i++) {
-                if (sessionStorage.getItem("Time") != null)
-                {
+                if (sessionStorage.getItem("Time") != null) {
                     if (data.Result[i].Timestamp > sessionStorage.Time) {
                         if (sessionStorage.Username == data.Result[i].Username) {
-                            $('#showMsg').append(data.Result[i].Message + '\n');
+                            $('.ass').append(data.Result[i].Message + '<br/>');
                             //sessionStorage.Time = data.Result[i].Timestamp;
                         }
                         else {
                             var date = new Date(parseInt(data.Result[i].Timestamp.substr(6)));
-                            $('#showMsg').append('\n' + data.Result[i].Username + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '\n' + data.Result[i].Message + '\n');
+                            $('.ass').append('<br/>' + data.Result[i].Username + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '<br/>' + data.Result[i].Message + '<br/>');
                             sessionStorage.Username = data.Result[i].Username;
                             //sessionStorage.Time = data.Result[i].Timestamp;
                         }
                     }
                 }
-                else
-                {
+                else {
                     if (sessionStorage.Username == data.Result[i].Username) {
-                        $('#showMsg').append(data.Result[i].Message + '\n');
+                        $('.ass').append(data.Result[i].Message + '<br/>');
                         //sessionStorage.Time = data.Result[i].Timestamp;
                     }
                     else {
                         var date = new Date(parseInt(data.Result[i].Timestamp.substr(6)));
-                        $('#showMsg').append('\n' + data.Result[i].Username + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '\n' + data.Result[i].Message + '\n');
+                        $('.ass').append('<br/>' + data.Result[i].Username + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '<br/>' + data.Result[i].Message + '<br/>');
                         sessionStorage.Username = data.Result[i].Username;
                         //sessionStorage.Time = data.Result[i].Timestamp;
                     }
-                }             
+                }
             }
             sessionStorage.Time = data.Result[data.Result.length - 1].Timestamp;
         }
@@ -57,8 +116,30 @@ $('#loadMsg').click(function () {
             alert("Error");
         }
     }, 'json');
+
+    setTimeout(function () {
+        
+        
+
+        if (isBottom)
+        {
+            div.scrollTop(div[0].scrollHeight);
+        }
+        else
+        {
+            div.scrollTop(div[0].scrollHeight);
+        }
+
+        
+    }, 35);
 });
 
+$(document).ready(function () {
+    $('#messageBox').keypress(function (e) {
+        if (e.keyCode == 13)
+            $('#sendMsgBtn').click();
+    });
+});
 
 //setInterval(function () {
 //    $.post('/Message/GetMessage/',
